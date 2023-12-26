91085caf05992f1a44162921853484c806e03c09
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-26 19:06:50 -0300

    app codepush

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {NativeBaseProvider} from 'native-base';
 import RootNavigation from 'navigation/RootNavigation';
 import {customTheme} from 'theme';
+import codePush from 'react-native-code-push';
 
 const App = () => {
   return (
@@ -11,4 +12,8 @@ const App = () => {
   );
 };
 
-export default App;
+export default codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.IMMEDIATE,
+  updateDialog: true,
+})(App);
