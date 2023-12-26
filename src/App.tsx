48b166c3d99c62c3dde653c953bf62d75b70e968
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-25 22:33:24 -0300

    Icons iOS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import * as React from 'react';
 
-import {View, Text} from 'react-native';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Inicio"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({color, size}) => <Icon name="home" size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Billetera"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({color, size}) => <Icon name="wallet" size={size} />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 const App = () => {
   return (
-    <View>
-      <Text>Hola</Text>
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 };
+
 export default App;
