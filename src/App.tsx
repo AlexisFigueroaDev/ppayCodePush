6ad4a7dea7139dac8738b5bba75c75d64e9e6185
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-26 10:54:11 -0300

    config ios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import * as React from 'react';
-
-import {Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome5';
 import RootNavigation from 'navigation/RootNavigation';
 
 const App = () => {
