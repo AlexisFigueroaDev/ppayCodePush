3e5cfdeed7509552c5409b234c433e5f30bf911a
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-27 14:06:03 -0300

    hook update

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -1,7 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import useCodePush from '@screens/CodePush/hook/UseCodePush';
 import UpdateApp from '@screens/CodePush/UpdateApp';
-import React from 'react';
+import React, {useState} from 'react';
 import CodePush from 'react-native-code-push';
 import AppStack from './AppStack/AppStack';
 import TabBar from './Tab-bar/TabBar';
@@ -9,17 +9,16 @@ import TabBar from './Tab-bar/TabBar';
 const RootNavigation: React.FC = () => {
   // const codePushUpdate = true;
   // const {progress, syncMessage} = useCodePush();
+  const [updatePending, setUpdatePending] = useState(false);
 
   const checkUpdate = async () => {
     const update = await CodePush.checkForUpdate();
-    return update;
+    return setUpdatePending(!!update);
   };
-
-  const update = checkUpdate();
 
   return (
     <NavigationContainer>
-      {update !== null || update !== undefined ? <UpdateApp /> : <AppStack />}
+      {updatePending ? <UpdateApp /> : <AppStack />}
     </NavigationContainer>
   );
 };
