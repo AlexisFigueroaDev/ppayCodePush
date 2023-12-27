f66903ded9437c3575633ed1140c004bd40b3aea
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-27 14:14:18 -0300

    hook update

--- src/screens/PrepaidCard/PrepaidCard-Home.tsx
+++ src/screens/PrepaidCard/PrepaidCard-Home.tsx
@@ -27,7 +27,10 @@ const PrepaidCardHome = () => {
         </FullWidthSolidButton>
       </Box>
 
-      <Backdrop isOpen={open} onClose={handleBackdrop} title={'Resultado'}>
+      <Backdrop
+        isOpen={open}
+        onClose={handleBackdrop}
+        title={'Nuevo Resultado'}>
         <Box marginY={2}>
           <Text variant="body-sm">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
