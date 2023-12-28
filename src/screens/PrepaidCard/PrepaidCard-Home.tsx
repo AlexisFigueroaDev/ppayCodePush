42d2a7a5120ea4fb76e14039a0266e6e0d00c7e1
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-28 14:13:07 -0300

    title iOS

--- src/screens/PrepaidCard/PrepaidCard-Home.tsx
+++ src/screens/PrepaidCard/PrepaidCard-Home.tsx
@@ -27,7 +27,7 @@ const PrepaidCardHome = () => {
         </FullWidthSolidButton>
       </Box>
 
-      <Backdrop isOpen={open} onClose={handleBackdrop} title={'Hola mundo 1'}>
+      <Backdrop isOpen={open} onClose={handleBackdrop} title={'Hola IOS'}>
         <Box marginY={2}>
           <Text variant="body-sm">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
