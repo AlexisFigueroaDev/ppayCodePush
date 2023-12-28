f844fa3c7c8c373c8f5bf86c8ef5b4a7355d8670
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-28 13:05:19 -0300

    title

--- src/screens/PrepaidCard/PrepaidCard-Home.tsx
+++ src/screens/PrepaidCard/PrepaidCard-Home.tsx
@@ -27,10 +27,7 @@ const PrepaidCardHome = () => {
         </FullWidthSolidButton>
       </Box>
 
-      <Backdrop
-        isOpen={open}
-        onClose={handleBackdrop}
-        title={'Resultado nuevo!'}>
+      <Backdrop isOpen={open} onClose={handleBackdrop} title={'Hola mundo'}>
         <Box marginY={2}>
           <Text variant="body-sm">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
