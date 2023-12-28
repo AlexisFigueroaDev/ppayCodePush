72264c273103643365e634afc9cb178b54512af8
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-28 19:14:36 -0300

    reverse

--- src/screens/CodePush/hook/UseCodePush.ts
+++ src/screens/CodePush/hook/UseCodePush.ts
@@ -2,8 +2,8 @@ import {useEffect, useState} from 'react';
 
 import SplashScreen from 'react-native-bootsplash';
 import codePush, {DownloadProgress} from 'react-native-code-push';
-import {CodePushDownloadProgressHandler} from './handleCodePushProgress';
-import {CodePushSyncStatusHandler} from './HandleCodePushStatus';
+// import {CodePushDownloadProgressHandler} from './handleCodePushProgress';
+// import {CodePushSyncStatusHandler} from './HandleCodePushStatus';
 
 interface UseCodePushReturn {
   syncMessage?: string;
@@ -22,54 +22,54 @@ const useCodePush = (): UseCodePushReturn => {
   const [syncStatusPush, setSyncStatusPush] = useState<unknown>();
   const [statusSelected, setStatusSelected] = useState<unknown>();
 
-  const codePushSyncHandler = new CodePushSyncStatusHandler(
-    (message: string | undefined) => {
-      // Implementa tu lógica para setSyncMessage
-      setSyncMessage(message);
-    },
-  );
+  // const codePushSyncHandler = new CodePushSyncStatusHandler(
+  //   (message: string | undefined) => {
+  //     // Implementa tu lógica para setSyncMessage
+  //     setSyncMessage(message);
+  //   },
+  // );
 
-  const codePushDownloadProgressHandler = new CodePushDownloadProgressHandler(
-    progress => {
-      // Lógica para setProgress
-      setProgress(progress);
-    },
-  );
+  // const codePushDownloadProgressHandler = new CodePushDownloadProgressHandler(
+  //   progress => {
+  //     // Lógica para setProgress
+  //     setProgress(progress);
+  //   },
+  // );
 
-  // const syncStatusChangedCallback = (syncStatus: codePush.SyncStatus) => {
-  //   switch (syncStatus) {
-  //     case codePush.SyncStatus.CHECKING_FOR_UPDATE:
-  //       setSyncMessage('Checking for update...');
-  //       break;
-  //     case codePush.SyncStatus.DOWNLOADING_PACKAGE:
-  //       setSyncMessage('Downloading update...');
-  //       break;
-  //     case codePush.SyncStatus.AWAITING_USER_ACTION:
-  //       setSyncMessage('User waiting...');
-  //       break;
-  //     case codePush.SyncStatus.INSTALLING_UPDATE:
-  //       setSyncMessage('Loading update...');
-  //       break;
-  //     case codePush.SyncStatus.UP_TO_DATE:
-  //       setSyncMessage('The app is up to date...');
-  //       break;
-  //     case codePush.SyncStatus.UPDATE_IGNORED:
-  //       setSyncMessage('Update canceled by user...');
-  //       break;
-  //     case codePush.SyncStatus.UPDATE_INSTALLED:
-  //       setSyncMessage('Update installed, Application restarting...');
-  //       break;
-  //     case codePush.SyncStatus.SYNC_IN_PROGRESS:
-  //       setSyncMessage('Sync in progress...');
-  //       break;
-  //     case codePush.SyncStatus.UNKNOWN_ERROR:
-  //       setSyncMessage('An error occurred during the update...');
-  //       break;
-  //     default:
-  //       setSyncMessage(undefined);
-  //       break;
-  //   }
-  // };
+  const syncStatusChangedCallback = (syncStatus: codePush.SyncStatus) => {
+    switch (syncStatus) {
+      case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+        setSyncMessage('Checking for update...');
+        break;
+      case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+        setSyncMessage('Downloading update...');
+        break;
+      case codePush.SyncStatus.AWAITING_USER_ACTION:
+        setSyncMessage('User waiting...');
+        break;
+      case codePush.SyncStatus.INSTALLING_UPDATE:
+        setSyncMessage('Loading update...');
+        break;
+      case codePush.SyncStatus.UP_TO_DATE:
+        setSyncMessage('The app is up to date...');
+        break;
+      case codePush.SyncStatus.UPDATE_IGNORED:
+        setSyncMessage('Update canceled by user...');
+        break;
+      case codePush.SyncStatus.UPDATE_INSTALLED:
+        setSyncMessage('Update installed, Application restarting...');
+        break;
+      case codePush.SyncStatus.SYNC_IN_PROGRESS:
+        setSyncMessage('Sync in progress...');
+        break;
+      case codePush.SyncStatus.UNKNOWN_ERROR:
+        setSyncMessage('An error occurred during the update...');
+        break;
+      default:
+        setSyncMessage(undefined);
+        break;
+    }
+  };
 
   const downloadProgressCallback = ({
     receivedBytes,
@@ -95,7 +95,8 @@ const useCodePush = (): UseCodePushReturn => {
               installMode: codePush.InstallMode.IMMEDIATE,
               minimumBackgroundDuration: 600,
             },
-            codePushSyncHandler.syncStatusChanged.bind(codePushSyncHandler),
+            syncStatusChangedCallback,
+            // codePushSyncHandler.syncStatusChanged.bind(codePushSyncHandler),
             downloadProgressCallback,
             // codePushDownloadProgressHandler.downloadProgressCallback.bind(
             //   codePushDownloadProgressHandler,
